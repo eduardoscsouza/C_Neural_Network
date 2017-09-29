c46d89c7b037a7881e030d3d7a774486d350fb11
eduardoscsouza <eduardoscsouza@gmail.com>
2017-09-29 18:09:33 -0300

    Finish Tests

--- main.c
+++ main.c
@@ -14,6 +14,11 @@
 #define TEST_NETWORK_LAYERS_ACTVS {&relu, &sigm, &relu, &sigm, &sigm}
 #define TEST_NETWORK_IN_SIZE 6
 
+#define TEST_XOR_N_LAYERS 2
+#define TEST_XOR_LAYERS_SIZES {2, 1}
+#define TEST_XOR_LAYERS_ACTVS {&step, &step}
+#define TEST_XOR_IN_SIZE 2
+
 
 
 nn_float_t * rand_vect(int n)
@@ -81,6 +86,39 @@ int main(int argc, char * argv[])
 	free(in);
 	free(out);
 	printf("\n\n");
+
+
+
+	nn_size_t xor_layers_sizes[] = TEST_XOR_LAYERS_SIZES;
+	nn_float_t (*xor_layers_actvs[])(nn_float_t) = TEST_XOR_LAYERS_ACTVS;
+	network = new_network(TEST_XOR_N_LAYERS, xor_layers_sizes, xor_layers_actvs, TEST_XOR_IN_SIZE);
+	
+	network->layers[0]->neurons[0]->weights[0] = network->layers[0]->neurons[0]->weights[1] = 1.0;
+	network->layers[0]->neurons[0]->weights[2] = -0.5;
+	network->layers[0]->neurons[1]->weights[0] = network->layers[0]->neurons[1]->weights[1] = -1.0;
+	network->layers[0]->neurons[1]->weights[2] = 1.5;
+	network->layers[1]->neurons[0]->weights[0] = network->layers[1]->neurons[0]->weights[1] = 1.0;
+	network->layers[1]->neurons[0]->weights[2] = -1.5;
+	print_network(network);
+
+	in = rand_vect(network->in_size);
+	int i;
+	for (i=0; i<4; i++){
+		in[0] = (nn_float_t)(i&1);
+		in[1] = (nn_float_t)((i&2)>>1);
+		printf("Input: ");
+		print_vect(in, network->in_size);
+		
+		out = network_forward(network, in);
+		printf("Output: ");
+		print_vect(out, network->layers[network->n_layers-1]->n_neurons);
+		free(out);
+	}
+
+	delete_network(network);
+	free(in);
+
+
 
 	return 0;
 }
